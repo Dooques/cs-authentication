e716ca7e4243e04b39423b9eeaea0e34f2aff295
Sam <samuelgdukes@gmail.com>
2026-05-01 09:25:56 +0100

    Create attendee and speaker classes

---
+++ ConferenceManager/Model/Models/Attendee.cs
@@ -0,0 +1,9 @@
+﻿namespace ConferenceManager.Model.Models
+{
+    public class Attendee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Events { get; set; }
+    }
+}
---
+++ ConferenceManager/Model/Models/Speaker.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConferenceManager.Model.Models
+{
+    public class Speaker
+    {
+        [JsonPropertyName("id")]
+        int Id { get; set; }
+        
+        [JsonPropertyName("name")]
+        string Name { get; set; }
+
+        int EventId { get; set; }
+    }
+}
